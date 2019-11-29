48838ba283f24468ceb98b51a9c2700da2c9fa73
Manuelpv17 <58040245+Manuelpv17@users.noreply.github.com>
2019-11-28 20:40:11 -0500

    Add files via upload

--- behavior.js
+++ behavior.js
@@ -8,18 +8,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             thumbnailElement.className = "";
         }
       });
-
-      var disqus_config = function () {
-        this.page.url = http://107.20.128.217/index.html;  
-        this.page.identifier = Home;
-    };
-
-    (function () { // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        s.src = 'https://self-driving-cars.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
         
 
 });
