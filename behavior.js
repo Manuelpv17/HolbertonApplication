563f757a4f5778cafaed08eee1d45dc656b7b56c
Manuelpv17 <58040245+Manuelpv17@users.noreply.github.com>
2019-12-02 21:26:02 -0500

    Add files via upload

--- behavior.js
+++ behavior.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var thumbnailElement = document.getElementById("smart_thumbnail");
-    thumbnailElement.addEventListener("click", function() {
-        
-        if (thumbnailElement.className == "") {
-            thumbnailElement.className = "small";
-        }else{
-            thumbnailElement.className = "";
-        }
-      });
-        
+document.addEventListener("DOMContentLoaded", function (event) {
 
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    if (thumbnailElement != null) {
+        thumbnailElement.addEventListener("click", function () {
+
+            if (thumbnailElement.className == "") {
+                thumbnailElement.className = "small";
+            } else {
+                thumbnailElement.className = "";
+            }
+        });
+    }
 });
+
